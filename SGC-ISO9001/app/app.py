54bdf0e9fe456e18fae28e42cad782e392b9fb5c
messironaldo20059-dotcom <messironaldo20059@gmail.com>
2025-09-15 23:47:15 +0000

    inicio de sesion

--- SGC-ISO9001/app/app.py
+++ SGC-ISO9001/app/app.py
@@ -406,44 +406,6 @@ def capacitacion():
     conn.close()
     return render_template('capacitacion.html', capacitacion=datos)
 
-if __name__ == '__main__':
-
-# Checklist ISO 9001 (4.1 a 10.3)
-@app.route('/checklist-iso', methods=['GET', 'POST'])
-@login_required
-def checklist_iso():
-    if request.method == 'POST':
-        usuario = session.get('username', 'admin')
-        campos = [
-            'c4_1', 'c4_2', 'c4_3', 'c4_4',
-            'c5_1', 'c5_2', 'c5_3',
-            'c6_1', 'c6_2', 'c6_3',
-            'c7_1', 'c7_2', 'c7_3', 'c7_4', 'c7_5',
-            'c8_1', 'c8_2', 'c8_3', 'c8_4', 'c8_5', 'c8_6', 'c8_7',
-            'c9_1', 'c9_2', 'c9_3',
-            'c10_1', 'c10_2', 'c10_3'
-        ]
-        valores = [int(request.form.get(campo, 0)) for campo in campos]
-        fecha = datetime.now().strftime('%Y-%m-%d')
-        conn = sqlite3.connect(DB_PATH)
-        c = conn.cursor()
-        c.execute('''INSERT INTO checklist_iso (
-            usuario, c4_1, c4_2, c4_3, c4_4, c5_1, c5_2, c5_3, c6_1, c6_2, c6_3, c7_1, c7_2, c7_3, c7_4, c7_5,
-            c8_1, c8_2, c8_3, c8_4, c8_5, c8_6, c8_7, c9_1, c9_2, c9_3, c10_1, c10_2, c10_3, fecha
-        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',
-            [usuario] + valores + [fecha]
-        )
-        conn.commit()
-        conn.close()
-        return redirect(url_for('checklist_iso'))
-    # Mostrar checklist guardadas
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    c.execute('SELECT * FROM checklist_iso ORDER BY fecha DESC')
-    checklist = c.fetchall()
-    conn.close()
-    return render_template('checklist_iso.html', checklist=checklist)
-
 
 # Checklist ISO 9001 (4.1 a 10.3)
 @app.route('/checklist-iso', methods=['GET', 'POST'])

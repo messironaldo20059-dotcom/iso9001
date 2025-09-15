95cb9c57e8ed461961b636bd726afed657328d29
messironaldo20059-dotcom <messironaldo20059@gmail.com>
2025-09-15 23:01:47 +0000

    inicio de sesion

--- SGC-ISO9001/app/app.py
+++ SGC-ISO9001/app/app.py
@@ -1,9 +1,3 @@
- 
-# Ruta para formulario de Registro ISO
-@app.route('/registro-iso', methods=['GET'])
-@login_required
-def registro_iso():
-    return render_template('registro_iso.html')
 
 
 from flask import Flask, render_template, request, redirect, url_for, Response, session
